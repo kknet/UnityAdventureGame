c333e1aff41d57d8671722a8b14685e5014e02a0
Rishi-Amarnani <rsaamar@gmail.com>
2017-06-30 16:51:13 -0500

    Added sheathing

--- TryingBlenderAnim3/Assets/scripts/WeaponToggle.cs
+++ TryingBlenderAnim3/Assets/scripts/WeaponToggle.cs
@@ -4,32 +4,65 @@ using UnityEngine;
 
 public class WeaponToggle : MonoBehaviour {
 
-	GameObject scimitar;
 	GameObject[] allWeps;
+	Dictionary <string, GameObject> weaponsTable;
+	public string weaponOut;
+	public Animator myAnimator;
 
 	// Use this for initialization
 	void Start () {
+		weaponsTable = new Dictionary<string, GameObject> ();
 		allWeps = GameObject.FindGameObjectsWithTag ("Weapons");
-		scimitar = GameObject.Find ("Scimitar");
+		myAnimator = GameObject.Find ("DevDrake").GetComponent<Animator> ();
+		weaponOut = "Scimitar";
+		initTable ();
 	}
-	
+
+	void initTable(){
+		foreach (GameObject g in allWeps) {
+			weaponsTable.Add (g.name, g);
+		}
+		if(weaponOut!="")
+			weaponsTable[weaponOut + "In"].SetActive(false);
+	}
+
+
 	// Update is called once per frame
 	void Update () {	
-		if (Input.GetKeyDown(KeyCode.Alpha1)) {	
-			setAllInactive ();
-			scimitar.SetActive (true);
+		if (Input.GetKeyDown(KeyCode.Alpha0)) {
+			StartSheath ();
 		}
-		else if (Input.GetKeyDown(KeyCode.Alpha0)) {
-			setAllInactive ();
-		}
-
+//		else if (Input.GetKeyDown(KeyCode.Alpha1)) {	
+//			if (weaponOut == "Scimitar")
+//				return;
+//			weaponOut = "Scimitar";
+//		}
 	}
 
-	void setAllInactive()
-	{
-//		allWeps = GameObject.FindGameObjectsWithTag ("Weapons");
-		foreach (GameObject g in allWeps) {
-			g.SetActive (false);
-		}		
+
+
+	void StartSheath() {
+		if(myAnimator.GetBool("WeaponDrawn"))
+			myAnimator.SetBool ("Sheathing", true);
 	}
+	public void FinishSheath(){
+		weaponsTable[weaponOut + "Out"].SetActive(false);
+		weaponsTable[weaponOut + "In"].SetActive(true);
+		weaponOut = "";
+		myAnimator.SetBool ("Sheathing", false);
+		myAnimator.SetBool(("WeaponDrawn"), false);
+	}
+
+//	void StartDraw(){
+//		GameObject.Find (weaponOut + "Out").SetActive(true);
+//		myAnimator.SetBool ("Sheathing", true);
+//	}
+	
+//	void setAllInactive()
+//	{
+////		allWeps = GameObject.FindGameObjectsWithTag ("Weapons");
+//		foreach (GameObject g in allWeps) {
+//			g.SetActive (false);
+//		}		
+//	}
 }
