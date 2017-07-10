4a9126396f7ff33ad9a9a79ec56b6ffe7fbe88f7
Rishi-Amarnani <rsaamar@gmail.com>
2017-07-09 19:34:54 -0500

    Added Blocking and Modified Shield
    Added the ability to block by holding the right mouse button, and
    increased the size of the shield to make it more effective in combat.

---
+++ TryingBlenderAnim3/Assets/scripts/DevCombat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevCombat : MonoBehaviour {
+	public Animator myAnimator;
+
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+//			myAnimator.SetBool ("doAttack", true);
+//			Invoke ("stopAttack", 0.3f);
+		} else if (Input.GetKey (KeyCode.Mouse1)) {
+			myAnimator.SetBool ("isBlocking", true);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Mouse1)) {
+			myAnimator.SetBool ("isBlocking", false);
+		}
+
+	}
+
+	void stopAttack(){
+		myAnimator.SetBool ("doAttack", false);
+	}
+}
