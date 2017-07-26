b34613baf4e04238f5ec526bc0a53887bb9f1b42
Rishi-Amarnani <rsaamar@gmail.com>
2017-07-26 17:40:16 -0500

    Enemy attack player once in range

--- TryingBlenderAnim3/Assets/scripts/EnemyAI.cs
+++ TryingBlenderAnim3/Assets/scripts/EnemyAI.cs
@@ -37,8 +37,54 @@ public class EnemyAI : MonoBehaviour {
 //			reduceSpeed ();
 //		}
 
+		if (!isEnemyAttacking() && Vector3.Magnitude (Dev.transform.position - transform.position) < 2f) {
+			attack ();
+		}
+
 
 	}
+
+	public bool isEnemyAttacking() {
+		AnimatorStateInfo info = enemyAnim.GetCurrentAnimatorStateInfo (0);
+		return info.IsName ("QUICK1") || info.IsName ("QUICK2") || info.IsName ("QUICK3") || info.IsName ("QUICK4") || info.IsName ("QUICK5");
+	}
+
+
+	private void attack() {
+		enemyAnim.SetBool ("enemyAttack", true);
+		Invoke ("switchAttack", 0.5f);
+	}
+
+	public void stopEnemyAttack(){
+		enemyAnim.SetBool ("enemyAttack", false);
+	}
+
+
+	private void switchAttack(){
+		switch (enemyAnim.GetInteger ("enemyQuick")) {
+		case 1:
+			enemyAnim.SetInteger ("enemyQuick", 2);
+			break;
+		case 2:
+			enemyAnim.SetInteger ("enemyQuick", 3);
+			break;
+		case 3:
+			enemyAnim.SetInteger ("enemyQuick", 4);
+			break;
+		case 4:
+			enemyAnim.SetInteger ("enemyQuick", 5);
+			break;
+		case 5:
+			enemyAnim.SetInteger ("enemyQuick", 1);
+			break;
+		default:
+			Debug.LogAssertion ("quickAttack is not set to 1-5, look at EnemyAI.cs script (switchAttack method)");
+			break;
+		}
+	}
+
+
+
 	private float rand(float a, float b){
 		return UnityEngine.Random.Range (a, b);
 	}
@@ -50,7 +96,7 @@ public class EnemyAI : MonoBehaviour {
 	private void applyRotation() {
 		//if not moving
 		if (Mathf.Approximately (enemyAnim.GetFloat ("enemySpeed"), 0f)) {
-			if ((oldDev == Vector3.zero) || (Vector3.Magnitude (oldDev - Dev.transform.position) > 2f && Vector3.Magnitude (Dev.transform.position - transform.position) > 2f)) {
+			if ((oldDev == Vector3.zero) || (Vector3.Magnitude (oldDev - Dev.transform.position) > 0.7f && Vector3.Magnitude (Dev.transform.position - transform.position) > 0.7f)) {
 				dif = Dev.transform.position - transform.position;
 				dif = new Vector3 (dif.x, 0f, dif.z);
 				Vector3 perpenDif = Vector3.Normalize (Vector3.Cross (dif, -1.0f * dif)) * rand (1f, -1f);
@@ -61,7 +107,7 @@ public class EnemyAI : MonoBehaviour {
 			}		
 		//if moving
 		} else {
-			if ((Vector3.Magnitude (oldDev - Dev.transform.position) > 2f && Vector3.Magnitude (Dev.transform.position - transform.position) > 2f)) {
+			if ((Vector3.Magnitude (oldDev - Dev.transform.position) > 0.7f && Vector3.Magnitude (Dev.transform.position - transform.position) > 0.7f)) {
 				dif = Dev.transform.position - transform.position;
 				dif = new Vector3 (dif.x, 0f, dif.z);
 				Vector3 perpenDif = Vector3.Normalize (Vector3.Cross (dif, -1.0f * dif)) * rand (1f, -1f);
@@ -79,8 +125,8 @@ public class EnemyAI : MonoBehaviour {
 	}
 
 	private void applySpeed() {
-		if (Vector3.Magnitude (dif) < 2f) {
-			enemyAnim.SetFloat ("enemySpeed", Mathf.MoveTowards(enemyAnim.GetFloat ("enemySpeed"), 0f, 2f * Time.deltaTime));
+		if (Vector3.Magnitude (dif) < 1f) {
+			enemyAnim.SetFloat ("enemySpeed", Mathf.MoveTowards(enemyAnim.GetFloat ("enemySpeed"), 0f, 5f * Time.deltaTime));
 		} else {
 			enemyAnim.SetFloat ("enemySpeed", Mathf.MoveTowards(enemyAnim.GetFloat ("enemySpeed"), 1f, 2f * Time.deltaTime));
 		}
