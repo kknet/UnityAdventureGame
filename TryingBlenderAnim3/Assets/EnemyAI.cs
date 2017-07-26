5c341d57d02727cbb7194c433f590b8f287b2ee8
Rishi-Amarnani <rsaamar@gmail.com>
2017-07-26 16:33:18 -0500

    Enemy moves to player WITH SOME RANDOM VARIANCE

--- TryingBlenderAnim3/Assets/EnemyAI.cs
+++ TryingBlenderAnim3/Assets/EnemyAI.cs
@@ -9,21 +9,28 @@ public class EnemyAI : MonoBehaviour {
 	private float rotSpeed;
 	private float moveSpeed;
 	private Vector3 dif;
+	private Vector3 oldDev;
+	private Vector3 target;
+	private Vector3 devTarget;
+
 	// Use this for initialization
 	void Start () {
 		enemyAnim = GetComponent<Animator> ();
 		Dev = GameObject.Find ("DevDrake");
 		rotSpeed = 5f;
 		moveSpeed = 3f;
+		oldDev = Vector3.zero;
+		target = Vector3.zero;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (Dev.transform.position + " " + transform.position + " " + dif);
+		Debug.Log ("oldDev: "+ oldDev + " dev: "+ Dev.transform.position + " enemy: " + transform.position + " dif: " + dif);
 
 		applyRotation ();
 
 		if (!Approx (dif, Vector3.zero)) {
+			oldDev = devTarget;
 			applySpeed ();
 		} 
 //			else {
@@ -32,19 +39,46 @@ public class EnemyAI : MonoBehaviour {
 
 
 	}
+	private float rand(float a, float b){
+		return UnityEngine.Random.Range (a, b);
+	}
 
 	private bool Approx(Vector3 a, Vector3 b){
 		return Mathf.Approximately (a.x, b.x) && Mathf.Approximately (a.y, b.y) && Mathf.Approximately (a.z, b.z);
 	}
 
 	private void applyRotation() {
-		dif = Dev.transform.position - transform.position;
-		dif = new Vector3 (dif.x, 0f, dif.z);
+		//if not moving
+		if (Mathf.Approximately (enemyAnim.GetFloat ("enemySpeed"), 0f)) {
+			if ((oldDev == Vector3.zero) || (Vector3.Magnitude (oldDev - Dev.transform.position) > 2f && Vector3.Magnitude (Dev.transform.position - transform.position) > 2f)) {
+				dif = Dev.transform.position - transform.position;
+				dif = new Vector3 (dif.x, 0f, dif.z);
+				Vector3 perpenDif = Vector3.Normalize (Vector3.Cross (dif, -1.0f * dif)) * rand (1f, -1f);
+				devTarget = Dev.transform.position;
+				target = Dev.transform.position + perpenDif;
+				dif = target - transform.position;
+				dif = new Vector3 (dif.x, 0f, dif.z);
+			}		
+		//if moving
+		} else {
+			if ((Vector3.Magnitude (oldDev - Dev.transform.position) > 2f && Vector3.Magnitude (Dev.transform.position - transform.position) > 2f)) {
+				dif = Dev.transform.position - transform.position;
+				dif = new Vector3 (dif.x, 0f, dif.z);
+				Vector3 perpenDif = Vector3.Normalize (Vector3.Cross (dif, -1.0f * dif)) * rand (1f, -1f);
+				devTarget = Dev.transform.position;
+				target = Dev.transform.position + perpenDif;
+				dif = target - transform.position;
+				dif = new Vector3 (dif.x, 0f, dif.z);
+			} else {
+				dif = target - transform.position;
+				dif = new Vector3 (dif.x, 0f, dif.z);
+			}
+		}
 
 		transform.forward = Vector3.RotateTowards (transform.forward, dif, rotSpeed * Time.deltaTime, 0.0f); 
 	}
 
-	private void applySpeed(){
+	private void applySpeed() {
 		if (Vector3.Magnitude (dif) < 2f) {
 			enemyAnim.SetFloat ("enemySpeed", Mathf.MoveTowards(enemyAnim.GetFloat ("enemySpeed"), 0f, 2f * Time.deltaTime));
 		} else {
--- TryingBlenderAnim3/Assets/scripts/EmptyClass2.cs
+++ TryingBlenderAnim3/Assets/scripts/EmptyClass2.cs
@@ -1,77 +1,61 @@
-﻿//using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 //
-//public class Orbit : MonoBehaviour {
+//public class EnemyAI : MonoBehaviour {
 //
-//	private bool firstTimeAdjust;
-//	public float sensitivityX;
-//	public float sensitivityY;
-//	public GameObject player;
-//
-//	[SerializeField][HideInInspector]
-//	private Vector3 initialOffset;
-//	private Vector3 currentOffset;
-//
-//	[ContextMenu("Set Current Offset")]
-//	private void SetCurrentOffset () {
-//		initialOffset = transform.position - player.transform.position;
+//	private Animator enemyAnim;
+//	private GameObject Dev;
+//	private float rotSpeed;
+//	private float moveSpeed;
+//	private Vector3 dif;
+//	// Use this for initialization
+//	void Start () {
+//		enemyAnim = GetComponent<Animator> ();
+//		Dev = GameObject.Find ("DevDrake");
+//		rotSpeed = 5f;
+//		moveSpeed = 3f;
 //	}
 //
-//	private void Start () {
-//		if(player == null) {
-//			Debug.LogError ("Assign a player for the camera in Unity's inspector");
+//	// Update is called once per frame
+//	void Update () {
+//		Debug.Log (Dev.transform.position + " " + transform.position + " " + dif);
+//
+//		applyRotation ();
+//
+//		if (!Approx (dif, Vector3.zero)) {
+//			applySpeed ();
+//		} 
+//		//			else {
+//		//			reduceSpeed ();
+//		//		}
+//
+//
+//	}
+//
+//	private bool Approx(Vector3 a, Vector3 b){
+//		return Mathf.Approximately (a.x, b.x) && Mathf.Approximately (a.y, b.y) && Mathf.Approximately (a.z, b.z);
+//	}
+//
+//	private void applyRotation() {
+//		dif = Dev.transform.position - transform.position;
+//		dif = new Vector3 (dif.x, 0f, dif.z);
+//
+//		transform.forward = Vector3.RotateTowards (transform.forward, dif, rotSpeed * Time.deltaTime, 0.0f); 
+//	}
+//
+//	private void applySpeed(){
+//		if (Vector3.Magnitude (dif) < 2f) {
+//			enemyAnim.SetFloat ("enemySpeed", Mathf.MoveTowards(enemyAnim.GetFloat ("enemySpeed"), 0f, 2f * Time.deltaTime));
+//		} else {
+//			enemyAnim.SetFloat ("enemySpeed", Mathf.MoveTowards(enemyAnim.GetFloat ("enemySpeed"), 1f, 2f * Time.deltaTime));
 //		}
-//		currentOffset = initialOffset;
-//		firstTimeAdjust = false;
+//		transform.Translate(Vector3.forward * enemyAnim.GetFloat("enemySpeed") * Time.deltaTime * moveSpeed);
 //	}
 //
-//	private void VerticalRotation()  {
-//		float movementY = Input.GetAxis ("Mouse Y") * sensitivityY * Time.deltaTime;
-//		if (!Mathf.Approximately (movementY, 0f)) {
-//			float total = movementY + transform.rotation.eulerAngles.x;
-//			if (total > 180f)
-//				movementY -= 360f;
-//			else if (total < -180f)
-//				movementY += 360f;
-//			total = movementY + transform.rotation.eulerAngles.x;//recalculate total		
-//			total = Mathf.Clamp (total, -30f, 50f);//clamp it with limits
-//			transform.rotation = Quaternion.Euler (total, 0f, 0f);//calculate resulting quaternion
-//		}
-//	}
-//
-//	private void HorizontalRotation(){
-//		bool idle = player.GetComponent<DevAnimScript>().isIdle ();
-//		float movementX = Input.GetAxis ("Mouse X") * sensitivityX * Time.deltaTime;
-//		if (!Mathf.Approximately (movementX, 0f)) {
-//			if (idle) {
-//				//Debug.Log ("In Idle");
-//				transform.RotateAround (player.transform.position, Vector3.up, movementX);
-//				firstTimeAdjust = true;
-//			} else {
-//				//Debug.Log ("Not In Idle");
-//				if (player.GetComponent<DevAnimScript> ().adjustCounter == 0) {
-//					if (transform.rotation.eulerAngles.y != player.transform.rotation.eulerAngles.y) {
-//						float speed = (player.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y);
-//						transform.RotateAround (player.transform.position, Vector3.up, speed);	
-//					}
-//				}
-//			}
-//		}
-//		else if(!idle && (firstTimeAdjust || player.GetComponent<DevAnimScript> ().adjustCounter > 0))
-//		{
-//			//Debug.Log ("Need To Adjust");
-//			float dif = transform.rotation.eulerAngles.y - player.transform.rotation.eulerAngles.y;
-//			if (!Mathf.Approximately(dif,0)) {
-//				player.GetComponent<DevAnimScript>().adjustToCam (dif, firstTimeAdjust);
-//				firstTimeAdjust = false;
-//			}
-//		}
-//	}
+//	//	private void reduceSpeed(){
+//	//		enemyAnim.SetFloat ("enemySpeed", Mathf.Max (0f, enemyAnim.GetFloat ("enemySpeed") - 0.03f));
+//	//	}
 //
 //
-//	private void LateUpdate () {
-//		transform.position = player.transform.position + currentOffset;
-//		VerticalRotation ();
-//		HorizontalRotation ();
-//		currentOffset = transform.position - player.transform.position;
-//	}
 //}
