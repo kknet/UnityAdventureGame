43dd5f8bcb614c4962c54e0787a73d4f30e917ad
Rishi-Amarnani <rsaamar@gmail.com>
2017-08-23 12:51:32 -0500

    Finished skeleton code for grid graph

--- TryingBlenderAnim3/Assets/scripts/MapPathfind.cs
+++ TryingBlenderAnim3/Assets/scripts/MapPathfind.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MapPathfind : MonoBehaviour {
 
+	public mapNode[][] grid;
+
 	private float len;
 	private float wid;
 	private Terrain ter;
@@ -23,7 +25,36 @@ public class MapPathfind : MonoBehaviour {
 	}
 
 	void buildGridGraph(){
-	
+
+		//build each node
+		int sideLength = Mathf.Sqrt (numNodes);
+		grid = numNodes [sideLength] [sideLength];
+
+		for (int x = 0; x < sideLength; ++x) {
+			for (int y = 0; y < sideLength; ++y) {
+				grid [x] [y] = new mapNode (Vector3.zero);
+			}
+		}
+
+		//connect the nodes via setNeighbors, forming a grid graph
+		grid [0] [0].setNeighbors();
+		grid [0] [sideLength-1].setNeighbors();
+		grid [sideLength-1] [sideLength-1].setNeighbors();
+		grid [sideLength-1] [0].setNeighbors();
+
+		for (int x = 1; x < sideLength - 1; ++x) {
+			grid [0] [x].setNeighbors();
+			grid [sideLength-1] [x].setNeighbors();
+			grid [x] [0].setNeighbors();
+			grid [x] [sideLength-1].setNeighbors();
+		}
+
+		for (int x = 1; x < sideLength-1; ++x) {
+			for (int y = 1; y < sideLength-1; ++y) {
+				grid [x] [y].setNeighbors();
+			}
+		}
+
 	}
 	
 	// Update is called once per frame
@@ -44,27 +75,52 @@ public class MapPathfind : MonoBehaviour {
 
 public class mapNode { 
 
-	//format: 
-	// 0: (minX, minZ), 1: (minX, maxZ)
-	// 2 :(maxX, minZ), 3: (maxX, maxZ)
-	Vector3[] points;
+	//format for points: 
+	// 1: (minX, maxZ), 2: (maxX, maxZ)
+	// 0: (minX, minZ), 3: (maxX, minZ)
+	//edges are 0 to 1, 1 to 2, 2 to 3, and 3 to 0
+	//the 0,1,2,3 is the order of the points in the points[] array
 
-	public mapNode(Vector3[] pts) {
-		points = pts;
+	Vector3[] points;
+	Vector3 center;
+	mapNode[] neighbors;
+
+	public mapNode(Vector3 ctr) {
+		center = ctr;
+		points[0] = new Vector3(ctr.x - 1f, ctr.y, ctr.z - 1f);
+		points[1] = new Vector3(ctr.x - 1f, ctr.y, ctr.z + 1f);
+		points[2] = new Vector3(ctr.x + 1f, ctr.y, ctr.z + 1f);
+		points[3] = new Vector3(ctr.x + 1f, ctr.y, ctr.z - 1f);
+	}
+
+	public void setNeighbors(mapNode[] n){
+		neighbors = n;
+	}
+
+	public mapNode[] getNeighbors(){
+		return neighbors;
 	}
 
 	public Vector3 getCenter(){
-		float cenX = 0f;
-		float cenZ = 0f;
-		foreach(Vector3 pt in points){
-			cenX += pt[0];
-			cenZ += pt[2];
-		}
-		cenX = cenX/4f;
-		cenZ = cenZ/4f;
-		return new Vector3(cenX, 0f, cenZ);
+		return center;
 	}
 
+	public Vector3[] getPoints(){
+		return points;
+	}
+
+//	public Vector3 getCenter(){
+//		float cenX = 0f;
+//		float cenZ = 0f;
+//		foreach(Vector3 pt in points){
+//			cenX += pt[0];
+//			cenZ += pt[2];
+//		}
+//		cenX = cenX/4f;
+//		cenZ = cenZ/4f;
+//		return new Vector3(cenX, 0f, cenZ);
+//	}
+//
 //	public mapNode[] getNeighbors(){
 //		bool hasMinX = Mathf.Approximately (points [0][0], GameObject.Find ("Terrain").GetComponent<MapPathfind> ().min [0]);
 //		bool hasMaxX = Mathf.Approximately (points [4][0], GameObject.Find ("Terrain").GetComponent<MapPathfind> ().max [0]);
